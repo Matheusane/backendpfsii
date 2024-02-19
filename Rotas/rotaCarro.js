d2134e4fb449e95d902423d09f50d50b6da4aa5e
Your Name <you@example.com>
2024-02-18 21:46:22 -0300

    ajsutes

--- Rotas/rotaCarro.js
+++ Rotas/rotaCarro.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import carroCtrl from "../Controle/carroCtrl.js";
 
-const prodCtrl = new carroCtrl();
+const carCtrl = new carroCtrl();
 const rotaCarro = new Router();
 
 rotaCarro
-.get('/', prodCtrl.consultar)
-.get('/:termo', prodCtrl.consultar)
-.post('/', prodCtrl.gravar)
-.patch('/', prodCtrl.atualizar)
-.put('/', prodCtrl.atualizar)
-.delete('/', prodCtrl.excluir);
+.get('/', carCtrl.consultar)
+.get('/:termo', carCtrl.consultar)
+.post('/', carCtrl.gravar)
+.patch('/', carCtrl.atualizar)
+.put('/', carCtrl.atualizar)
+.delete('/', carCtrl.excluir);
 
 export default rotaCarro;
--- Rotas/rotaMarca.js
+++ Rotas/rotaMarca.js
@@ -4,15 +4,15 @@ import MarcaCtrl from "../Controle/marcaCtrl.js";
 //rotas é o mapeamento das requisições da web para um determinado
 //endpoint da aplicação
 
-const catCtrl = new MarcaCtrl();
+const marCtrl = new MarcaCtrl();
 const rotaMarca = new Router();
 
 rotaMarca
-.get('/',catCtrl.consultar)
-.get('/:termo', catCtrl.consultar)
-.post('/',catCtrl.gravar)
-.patch('/',catCtrl.atualizar)
-.put('/',catCtrl.atualizar)
-.delete('/',catCtrl.excluir);
+.get('/',marCtrl.consultar)
+.get('/:termo', marCtrl.consultar)
+.post('/',marCtrl.gravar)
+.patch('/',marCtrl.atualizar)
+.put('/',marCtrl.atualizar)
+.delete('/',marCtrl.excluir);
 
 export default rotaMarca;
