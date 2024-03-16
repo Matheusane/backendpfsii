851578e7423c4b0cc6e3d8c948082e0e8d95fb32
Your Name <you@example.com>
2024-03-16 13:15:14 -0300

    Ajuste Autorização

--- Rotas/rotaLogin.js
+++ Rotas/rotaLogin.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { autenticar } from "../Seguranca/autenticacao.js";
 
 const rotaLogin = new Router();
-console.log("oi")
 rotaLogin.post('/', (req, res) =>{ 
     autenticar(req, res) 
 })
